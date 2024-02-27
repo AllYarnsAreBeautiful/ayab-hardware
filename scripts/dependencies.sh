3d58ddc9970fdd0507a04542b45e8756fa211f42
matei jordache <mateijordache@live.com>
2024-02-27 18:59:03 -0500

    remove bash error

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-
-if [[ -z "${GITHUB_WORKFLOW}" ]]; then
-    >&2 echo "Aborting! This script is meant to be run in CI (Github Actions) only. It may modify/damage your system configuration if run outside of CI."
-    exit 1
-fi
-
 set -v
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
