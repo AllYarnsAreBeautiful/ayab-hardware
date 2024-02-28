3601e2d20fadc1663b073dd6326fd2a30347b6a5
matei jordache <mateijordache@live.com>
2024-02-28 09:18:03 -0500

    fix dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,3 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo add-apt-repository --yes ppa:kicad/kicad-8.0-releases
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt install --install-recommends kicad
+sudo DEBIAN_FRONTEND=noninteractive apt install python3 pip
+
+sudo python3 -m pip install pandas
