1b404f80a49ff8dfcfce6fe9a27d06f5be315ed9
Jonathan Perret <j-github@jonathanperret.net>
2025-05-10 23:09:53 +0200

    Install kicad-packages3d (required for renders)

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo add-apt-repository --yes ppa:kicad/kicad-9.0-releases
 sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt install --install-recommends kicad kicad-packages3d- kicad-demos-
-sudo DEBIAN_FRONTEND=noninteractive apt install python3 pip ghostscript
+sudo apt-get install -qy --install-recommends kicad kicad-packages3d kicad-demos-
+sudo apt-get install -qy python3 pip ghostscript
 
 sudo python3 -m pip install pandas
